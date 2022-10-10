c8f9b70b8b14ab92547c52b13ea142bc795df6b0
giaphuongd <giaphuongd@gmail.com>
2022-10-09 22:11:35 -0400

    Java Quiz 4 Zach Dang (danggia)

--- src/main/java/DrivableMap.java
+++ src/main/java/DrivableMap.java
@@ -27,7 +27,14 @@ class DrivableMap {
      *       in drivable_map, then add the pair to drivable_map.
      *       Return true if the Drivable was added to drivable_map.
      */
-
+    public boolean addDrivable(String id, Drivable object) {
+        if (drivable_map.containsKey(id)) {
+            return false;
+        } else {
+            drivable_map.put(id, object);
+            return true;
+        }
+    }
 
 
 
@@ -37,7 +44,14 @@ class DrivableMap {
      * You may want to use drivable_map.keys() or drivable_map.values() to
      * iterate through drivable_map.
      */
-
+    public boolean hasFasterThan(int speed) {
+        for (Drivable item: drivable_map.values()) {
+            if (item.getMaxSpeed() >= speed) {
+                return true;
+            }
+        }
+        return false;
+    }
 
 
 
@@ -46,8 +60,14 @@ class DrivableMap {
      *       returns a List containing all of the Tradable items in
      *       drivable_map.
      */
+    public List<Tradable> getTradable() {
+        ArrayList<Tradable> lst = new ArrayList<>();
+        for (Drivable item: drivable_map.values()) {
+            if (item instanceof Tradable) {
+                lst.add((Tradable) item);
+            }
+        }
+        return lst;
+    }
 
-
-
-    
 }
